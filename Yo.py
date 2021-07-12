d86a34cb1e52709b2fb1fec9919d0cbabf0d29c8
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-12 15:04:47 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -2,3 +2,4 @@ import shutil
 original = r'D:\\JenkinsHome\\workspace\\PipelineOne\\Hello.py'
 target = r'D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\Hello.exe'
 shutil.move(original,target)
+shutil.make_archive("Hello.exe","zip", root_dir)
