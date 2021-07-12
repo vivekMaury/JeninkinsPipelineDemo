edccb5b70c1084bc1dc7698154cc58fc1074aee0
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-12 15:26:53 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -3,3 +3,6 @@ original = r'D:\\JenkinsHome\\workspace\\PipelineOne\\Hello.py'
 target = r'D:\\JenkinsHome\\workspace\\PipelineOne\\Hello.exe'
 shutil.move(original,target)
 
+o = r'D:\\JenkinsHome\\workspace\\PipelineOne\\Hello.exe'
+t = r'D:\\JenkinsHome\\workspace\\PipelineOne\\[Release]\\Hello.zip'
+shutil.move(o,t)
