5657f0947b15b2c83c5645df1dc1e5141e9488e0
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-12 14:50:44 +0530

    Create Yo.py

---
+++ Yo.py
@@ -0,0 +1,4 @@
+import shutil
+original = r'D:\\JenkinsHome\\workspace\\PipelineOne\\Hello.py'
+target = r'D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\Hello.py'
+shutil.move(original,target)
