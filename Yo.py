e2907bfd628c2dc183840a57e77131fd4c1a6f96
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-12 14:52:52 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -1,4 +1,4 @@
 import shutil
 original = r'D:\\JenkinsHome\\workspace\\PipelineOne\\Hello.py'
-target = r'D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\Hello.py'
+target = r'D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\Hello.exe'
 shutil.move(original,target)
